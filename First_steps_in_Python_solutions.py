013d21fc3d2cef2476d54237afc05e0ca823e105
nevenaivanova <65981481+nevenaivanova@users.noreply.github.com>
2021-10-24 19:59:54 +0300

    Add files via upload

---
+++ First_steps_in_Python_solutions.py
@@ -0,0 +1,57 @@
+
+#Task 1
+print("Hello Mindhub")
+
+#Task 2 - Variant 1
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+print(6)
+print(7)
+print(8)
+print(9)
+print(10)
+
+#Task 2 - Variant 2
+for i in range(1,101,2):
+    print(i)
+
+
+a= int(input())
+b= int(input())
+area=a*b
+print(area)
+
+#Task 4 - Variant 2
+inches = int(input())
+print(inches*2.54)
+
+#Task 4 - Variant  1 
+inches = int(input())
+cm = inches*2.54
+print(cm)
+
+#Task 5
+name = str(input())
+print(f"Hello {name}!")
+
+#Task 6
+first_name = str(input())
+last_name = str(input())
+age = int(input())
+town = str(input())
+print (f"You are {first_name} {last_name}, a {age} -years old person from {town}.")
+
+#Task 7
+name=str(input())
+projects=int(input())
+all_hours= projects*3
+print(f"The architect {name} will need {all_hours} hours to complete {projects} project/s.")
+
+#Task 8
+cat_food = int(input())
+dog_food = int(input())
+sum= dog_food*2.50 + cat_food*4.00
+print(f"{sum} lv")
